1b7eab59403bbed50a765a7ab59c0b88805bdede
gabrielagabriela <gabrielaa.slv@gmail.com>
2024-04-13 09:45:52 -0300

    inicialização do projeto

---
+++ src/main/java/com/fullstack/education/labpcp/LabpcpApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.education.labpcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabpcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabpcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fullstack/education/labpcp/LabpcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.education.labpcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabpcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
