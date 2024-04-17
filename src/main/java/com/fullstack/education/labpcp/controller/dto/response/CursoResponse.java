faf5f0ed8dc0bca136ad2561fa24e0f651be948e
gabrielagabriela <gabrielaa.slv@gmail.com>
2024-04-17 14:56:31 -0300

    criação de entidade Curso, repository, response e request

---
+++ src/main/java/com/fullstack/education/labpcp/controller/dto/response/CursoResponse.java
@@ -0,0 +1,13 @@
+package com.fullstack.education.labpcp.controller.dto.response;
+
+public record CursoResponse(
+        Long id_curso,
+        String nome
+
+        /*
+        List<turmas>
+        List<materias>
+         */
+
+) {
+}
---
+++ src/main/java/com/fullstack/education/labpcp/datasource/repository/CursoRepository.java
@@ -0,0 +1,7 @@
+package com.fullstack.education.labpcp.datasource.repository;
+
+import com.fullstack.education.labpcp.datasource.entity.CursoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CursoRepository extends JpaRepository<CursoEntity, Long> {
+}
