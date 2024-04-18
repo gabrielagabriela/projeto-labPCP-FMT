ad7d41c440f19672385e65d7056cf1b7b4d17b3a
gabrielagabriela <gabrielaa.slv@gmail.com>
2024-04-18 16:35:07 -0300

    entidade, request e response de Materia

---
+++ src/main/java/com/fullstack/education/labpcp/controller/dto/request/MateriaRequest.java
@@ -0,0 +1,7 @@
+package com.fullstack.education.labpcp.controller.dto.request;
+
+public record MateriaRequest(
+        String nome,
+        String nomeCurso
+) {
+}
---
+++ src/main/java/com/fullstack/education/labpcp/datasource/entity/MateriaEntity.java
@@ -0,0 +1,22 @@
+package com.fullstack.education.labpcp.datasource.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "materia")
+public class MateriaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "materia_id")
+    private Long id;
+
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn(name = "curso_id")
+    private CursoEntity nomeCurso;
+
+}
