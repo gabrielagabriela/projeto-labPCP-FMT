4dc2bbcbd7846351efda18e6d26e1b186e06683c
gabrielagabriela <gabrielaa.slv@gmail.com>
2024-04-28 16:36:26 -0300

    ajustes exclusao de turma e exclusao de notas. ajuste readme

--- src/main/java/com/fullstack/education/labpcp/service/TurmaService.java
+++ src/main/java/com/fullstack/education/labpcp/service/TurmaService.java
@@ -1,6 +1,7 @@
 package com.fullstack.education.labpcp.service;
 
 import com.fullstack.education.labpcp.controller.dto.request.TurmaRequest;
+import com.fullstack.education.labpcp.controller.dto.response.TurmaComListAlunosResponse;
 import com.fullstack.education.labpcp.controller.dto.response.TurmaResponse;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -10,12 +11,12 @@ public interface TurmaService {
 
     TurmaResponse criarTurma(@RequestBody TurmaRequest turmaRequest, String token);
 
-    TurmaResponse obterTurmaPorId(Long id, String token);
+    TurmaComListAlunosResponse obterTurmaPorId(Long id, String token);
 
     TurmaResponse atualizarTurma(Long id, @RequestBody TurmaRequest turmaRequest, String token);
 
     void excluirTurma(Long id, String token);
 
-    List<TurmaResponse> listarTodosTurmas(String token);
+    List<TurmaComListAlunosResponse> listarTodosTurmas(String token);
 }
 
